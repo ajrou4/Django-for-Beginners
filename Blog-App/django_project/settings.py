3489f686ceca7486994bfb07b793dbf9920d0693
ajrou4 <mohamed1999ajro@gmail.com>
2025-05-31 04:41:38 +0100

    finshed this part and deployed

--- Blog-App/django_project/settings.py
+++ Blog-App/django_project/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS =[BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 
 
 # Default primary key field type
